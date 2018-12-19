03ff973b18a5c12016d0dcb8a3b25bbcfe7be475
Roman Popov <popov.r.k@yandex.ru>
2018-12-19 13:23:45 +0300

    Updates

---
+++ precache-manifest.fc05c777b4e610b8cfac3798fc22cac0.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "9428b2ee48e3d59b46b43c6671888547",
+    "url": "/static/media/responsive-design.9428b2ee.png"
+  },
+  {
+    "revision": "2fab15539f0139be961d",
+    "url": "/static/css/main.c61317ee.chunk.css"
+  },
+  {
+    "revision": "65d8fa79b97178e91422",
+    "url": "/static/js/1.65d8fa79.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40f1616d6e017173b898dcd72503735b",
+    "url": "/static/media/github.40f1616d.svg"
+  },
+  {
+    "revision": "ffec998def949369d6c0adc63fac73be",
+    "url": "/static/media/linkedin.ffec998d.svg"
+  },
+  {
+    "revision": "fa5d1cc675d15460e6b900a8a8628be7",
+    "url": "/static/media/telegram.fa5d1cc6.svg"
+  },
+  {
+    "revision": "601176bc4967f754a71d999c49e207d1",
+    "url": "/static/media/vkontakte.601176bc.svg"
+  },
+  {
+    "revision": "cba472257ff55294c59c991be3286870",
+    "url": "/static/media/about.cba47225.svg"
+  },
+  {
+    "revision": "da455ea4c3110d22cad9e838acdc8cee",
+    "url": "/static/media/udacity-cert.da455ea4.png"
+  },
+  {
+    "revision": "9a32362a3e25f5b4aadab1b0b9b60326",
+    "url": "/static/media/html5.9a32362a.svg"
+  },
+  {
+    "revision": "6700f5a46f7151da8f5003e99d2a773c",
+    "url": "/static/media/css3.6700f5a4.svg"
+  },
+  {
+    "revision": "b9fd7cc09d5729402bf13fa78f476b6f",
+    "url": "/static/media/js.b9fd7cc0.svg"
+  },
+  {
+    "revision": "ff4d984bb98b606bbb4ed4cbba63d571",
+    "url": "/static/media/react.ff4d984b.svg"
+  },
+  {
+    "revision": "5d2605d82596ece767a1d7d4b9e089b7",
+    "url": "/static/media/git.5d2605d8.svg"
+  },
+  {
+    "revision": "dd18390f9f0f65b16066723dcb5d0fd9",
+    "url": "/static/media/jquery.dd18390f.svg"
+  },
+  {
+    "revision": "2fab15539f0139be961d",
+    "url": "/static/js/main.2fab1553.chunk.js"
+  },
+  {
+    "revision": "d3c46768be8527d1b08d5b857402b677",
+    "url": "/static/media/gears.d3c46768.svg"
+  },
+  {
+    "revision": "aa36160039319d804dffbb5f162556f5",
+    "url": "/static/media/project-TimeTable.aa361600.jpg"
+  },
+  {
+    "revision": "9d34ace165aabe0182b83ff3da5955da",
+    "url": "/static/media/project-NeighborhoodMap.9d34ace1.jpg"
+  },
+  {
+    "revision": "17b158dbd2bf0f2602e26e59af68eee4",
+    "url": "/static/media/project-MyReads.17b158db.jpg"
+  },
+  {
+    "revision": "5eb06d4302fe96a6d79bd542be877512",
+    "url": "/static/media/project-DiamondRush.5eb06d43.jpg"
+  },
+  {
+    "revision": "40460f10897ab3e30f96b3ebfc8f84dd",
+    "url": "/static/media/project-RestaurantReviews.40460f10.jpg"
+  },
+  {
+    "revision": "15e0e8705c2b4108eb03cd35d34cf1a5",
+    "url": "/static/media/project-Testing.15e0e870.jpg"
+  },
+  {
+    "revision": "e1b8a1eef14e07966b7be63e3984bcec",
+    "url": "/static/media/project-MemoryGame.e1b8a1ee.jpg"
+  },
+  {
+    "revision": "15bc9421f88f9006413c6390eacdec4f",
+    "url": "/static/media/project-PixelArt.15bc9421.jpg"
+  },
+  {
+    "revision": "4b78bf9e16027b979fa019c05f380556",
+    "url": "/static/media/project-AnimalCard.4b78bf9e.jpg"
+  },
+  {
+    "revision": "7f314e91793c0d2e891961a98490d52b",
+    "url": "/static/media/contacts.7f314e91.svg"
+  },
+  {
+    "revision": "9962a1998038f14e77e9af37c1ac8635",
+    "url": "/static/media/gmail.9962a199.svg"
+  },
+  {
+    "revision": "007631122ce901b212a5d7973b6549df",
+    "url": "/static/media/arrow.00763112.svg"
+  },
+  {
+    "revision": "b07904c81681bc2320aaacdb5d8fdcee",
+    "url": "/static/media/background-image.b07904c8.jpg"
+  },
+  {
+    "revision": "c2eb6c488aab7848dbf551a2729968e4",
+    "url": "/index.html"
+  }
+];
